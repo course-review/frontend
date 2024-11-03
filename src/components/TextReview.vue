00769172059a0289d4a401c29ee41e40b826ccee
EvilBabyDemon <lteufelbe@ethz.ch>
2024-11-03 17:06:29 +0100

    functionality behind edit button

--- src/components/TextReview.vue
+++ src/components/TextReview.vue
@@ -1,26 +1,46 @@
 <script setup lang="ts">
-const { editable = false } = defineProps<{review: string, semester: string, editable?: boolean}>()
+import { ref } from 'vue';
+const { editable = false, review } = defineProps<{review: string, semester: string, editable?: boolean}>()
 
 function deleteReview() {
   console.log('Delete review')
 }
 
-function editReview() {
-  console.log('Edit review')
+const isEditing = ref(false);
+const reviewText = ref(review);
+
+function toggleEdit() {
+  isEditing.value = !isEditing.value;
+  //reset value when canceling
+  reviewText.value = review;
+}
+
+function submitEdit() {
+  // todo: handle submit
+  isEditing.value = false;
 }
 </script>
 
 <template>
   <v-card max-width="500" class="border mt-2">
     <v-card-text>
-      <div v-for="(block, index) in review.split('\n')" :key="index">
-        {{ block }}
-        <br v-if="block.length == 0" />
+      <div v-if="!isEditing">
+        <div v-for="(block, index) in review.split('\n')" :key="index">
+          {{ block }}
+          <br v-if="block.length == 0" />
+        </div>
       </div>
+      <v-textarea
+        v-else
+        v-model="reviewText"
+        rows="5"
+        auto-grow
+    ></v-textarea>
     </v-card-text>
     <v-card-subtitle v-if="!editable">{{ semester }}</v-card-subtitle>
     <v-card-actions v-if="editable">
-      <v-btn variant="tonal" color="orange" @click.stop="editReview">Edit</v-btn>
+      <v-btn variant="tonal" color="orange" @click.stop="toggleEdit">{{ isEditing ? 'Cancel' : 'Edit' }}</v-btn>
+      <v-btn v-if="isEditing" variant="tonal" color="green" @click.stop="submitEdit">Submit</v-btn>
       <v-btn variant="tonal" color="red" @click.stop="deleteReview">Delete</v-btn>
     </v-card-actions>
   </v-card>
