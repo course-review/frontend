8ab2f279481aff9e6517a6b0ad04f7f1b8e54aed
EvilBabyDemon <lteufelbe@ethz.ch>
2024-11-03 23:25:44 +0100

    always use textarea for userpage

--- src/components/TextReview.vue
+++ src/components/TextReview.vue
@@ -24,7 +24,7 @@ function submitEdit() {
 <template>
   <v-card max-width="500" class="border mt-2">
     <v-card-text>
-      <div v-if="!isEditing">
+      <div v-if="!editable">
         <div v-for="(block, index) in review.split('\n')" :key="index">
           {{ block }}
           <br v-if="block.length == 0" />
@@ -35,6 +35,7 @@ function submitEdit() {
         v-model="reviewText"
         rows="5"
         auto-grow
+        :readonly="!isEditing"
     ></v-textarea>
     </v-card-text>
     <v-card-subtitle v-if="!editable">{{ semester }}</v-card-subtitle>
