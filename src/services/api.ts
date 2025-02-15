c7b0e8ee4c8e139b21251ac14ceef8d76587f059
EvilBabyDemon <lteufelbe@ethz.ch>
2025-02-16 00:56:38 +0100

    more requests

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,17 @@
 import axios, { type AxiosInstance, type AxiosResponse } from 'axios';
 
 // Define the expected structure of the course data (adjust as needed)
-interface Course {
+export interface Course {
   CourseNumber: string;
   CourseName: string;
+  Date: string;
 }
+
+interface Review {
+  Review: string;
+  Semester: string;
+}
+
 
 // Create a typed Axios instance
 const API: AxiosInstance = axios.create({
@@ -16,5 +23,11 @@ export const coursesData = async (): Promise<AxiosResponse<Course[]>> => {
   return API.get<Course[]>('/courses');
 };
 
+export const fetchReviews = async (courseNumber: string): Promise<AxiosResponse<Review[]>> => {
+  return API.get<Review[]>(`/getReviews/${courseNumber}`);
+}
 
-// export const 
+
+export const fetchLatestReviews = async (): Promise<AxiosResponse<Course[]>> => {
+  return API.get<Course[]>('/latestReviews');
+}
