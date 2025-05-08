62a88fefa32fce42e1e5a5b0dc5166ebcb153450
EvilBabyDemon <75550946+EvilBabyDemon@users.noreply.github.com>
2025-05-09 00:07:47 +0200

    git diff and skeleton loading + formatting
    * added loading skeleton
    
    * only formatting
    
    * git diff for review changes

--- src/components/CheckAuth.vue
+++ src/components/CheckAuth.vue
@@ -1,28 +1,28 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { studentAuth } from "@/router/index";
+import { ref } from 'vue'
+import { studentAuth } from '@/router/index'
 
 // check if user is logged in
 const notLoggedIn = ref(false)
 async function setAuth() {
-    notLoggedIn.value = !await studentAuth()
+  notLoggedIn.value = !(await studentAuth())
 }
 setAuth()
 
 function redirectToLogin() {
-    const redirectUrl = `${window.location.origin}/tokenset`;
-    const originUrl = window.location.pathname;
-    const authorizationUrl = "https://n.ethz.ch/~lteufelbe/auth"; //todo set as env var
-    window.location.href = `${authorizationUrl}?redirect=${encodeURI(redirectUrl)}&origin=${encodeURI(originUrl)}`;
+  const redirectUrl = `${window.location.origin}/tokenset`
+  const originUrl = window.location.pathname
+  const authorizationUrl = 'https://n.ethz.ch/~lteufelbe/auth' //todo set as env var
+  window.location.href = `${authorizationUrl}?redirect=${encodeURI(redirectUrl)}&origin=${encodeURI(originUrl)}`
 }
 </script>
 
 <template>
-    <v-dialog v-model=notLoggedIn max-width="50%">
-        <v-card class="mx-auto" style="margin-top: 10px;">
-            <v-card-title>Not logged in!</v-card-title>
-            <v-card-text>Please log in to view your reviews and add reviews.</v-card-text>
-            <v-btn @click="redirectToLogin" class="ma-5" color="blue-lighten-1">Login</v-btn>
-        </v-card>
-    </v-dialog>
+  <v-dialog v-model="notLoggedIn" max-width="50%">
+    <v-card class="mx-auto" style="margin-top: 10px">
+      <v-card-title>Not logged in!</v-card-title>
+      <v-card-text>Please log in to view your reviews and add reviews.</v-card-text>
+      <v-btn @click="redirectToLogin" class="ma-5" color="blue-lighten-1">Login</v-btn>
+    </v-card>
+  </v-dialog>
 </template>
--- src/components/StarRating.vue
+++ src/components/StarRating.vue
@@ -1,25 +1,36 @@
 <script setup lang="ts">
-import { pushDeleteRating, pushUpdateRating } from '@/services/api';
+import { pushDeleteRating, pushUpdateRating } from '@/services/api'
 import RatingOverview from '../components/RatingOverview.vue'
 import type { Rating } from './Rating.types'
-import { computed, nextTick } from 'vue';
+import { computed, nextTick } from 'vue'
 
+const {
+  ratings,
+  editable = false,
+  isAdd = false,
+  ratingId = -1,
+  reloadData
+} = defineProps<{
+  ratings: { [key: string]: Rating }
+  ratingId?: number
+  editable?: boolean
+  isAdd?: boolean
+  reloadData?: () => any
+}>()
 
-const { ratings, editable = false, isAdd = false, ratingId = -1, reloadData} = defineProps<{ ratings: {[key: string]: Rating}, ratingId?: number, editable?: boolean, isAdd?: boolean, reloadData?: () => any }>()
+const localRatings = computed(() => ratings)
 
-const localRatings = computed(() => ratings);
-
-const ratingCategories: {[key: string]: string} = {
-  Recommended: "I would recommend it",
-  Engaging: "The content is interesting",
-  Difficulty: "The difficulty is appropriate",
-  Effort: "The required effort is appropriate",
-  Resources: "The resources are useful"
+const ratingCategories: { [key: string]: string } = {
+  Recommended: 'I would recommend it',
+  Engaging: 'The content is interesting',
+  Difficulty: 'The difficulty is appropriate',
+  Effort: 'The required effort is appropriate',
+  Resources: 'The resources are useful'
 }
 
 function updateRating(id: string, value: number) {
   console.log(`API call to update rating '${id}' to: ${value}`)
-  localRatings.value[id].rating = value;
+  localRatings.value[id].rating = value
   if (!isAdd) {
     pushUpdateRating(ratingId, localRatings.value)
   }
@@ -31,11 +42,11 @@ async function clearAllRatings() {
     if (reloadData != undefined) {
       nextTick(() => {
         reloadData()
-      });
+      })
     }
   }
   for (const key in localRatings.value) {
-    localRatings.value[key].rating = 0;
+    localRatings.value[key].rating = 0
   }
 }
 </script>
@@ -45,19 +56,31 @@ async function clearAllRatings() {
     <v-container v-for="(label, key) in ratingCategories" :key="key" class="pa-2">
       <v-row no-gutters>
         <v-col class="flex-grow-1 flex-shrink-0">
-          <v-card-text style="float: left; margin: auto;" class="pa-0 align-baseline">{{ label }}:</v-card-text>
+          <v-card-text style="float: left; margin: auto" class="pa-0 align-baseline"
+            >{{ label }}:</v-card-text
+          >
         </v-col>
         <v-col>
           <!-- todo: clearable field might be of interest -->
-          <v-rating class="ml-6 flex-shrink-1" :model-value="localRatings[key].rating " color="amber" :size="30" hover :readonly="!editable" @update:modelValue="updateRating(key as string, $event as number)"/>
+          <v-rating
+            class="ml-6 flex-shrink-1"
+            :model-value="localRatings[key].rating"
+            color="amber"
+            :size="30"
+            hover
+            :readonly="!editable"
+            @update:modelValue="updateRating(key as string, $event as number)"
+          />
         </v-col>
         <v-col class="flex-grow-0 flex-shrink-1">
-          <RatingOverview v-if="!editable" :ratingInformation="ratings[key]"/>
+          <RatingOverview v-if="!editable" :ratingInformation="ratings[key]" />
         </v-col>
       </v-row>
     </v-container>
     <v-card-actions v-if="editable">
-      <v-btn variant="flat" color="red-lighten-1" @click.stop="clearAllRatings">Clear all Ratings</v-btn>
+      <v-btn variant="flat" color="red-lighten-1" @click.stop="clearAllRatings"
+        >Clear all Ratings</v-btn
+      >
     </v-card-actions>
   </v-card>
 </template>
